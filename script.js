83c29eb69e8f091f058da424852ea9e123aa4ea7
geojuju <juliana.elizabeth.t@gmail.com>
2016-03-09 14:11:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-	var map;
-	require(["esri/map", "dojo/domReady!"], function(Map) {
-  		map = new Map("mapDiv", {
-    		center: [-97.7115, 30.2705],
-    		zoom: 12,
-    		basemap: "gray-vector"
-  		});
-	});
-	var mapid="fe9ef6970821469284403bfc72209e4b"
-	var mapDeferred = esri.arcgis.utils.createMap(mapid, "map", {
-  		mapOptions: {
-    		slider: true,
-    		nav:false
-  }
-});
-	var layers = response.itemInfo.itemData.operationalLayers;
-	var layerInfo = [];        
-		dojo.forEach(layers,function(layer){
-  	if(!layer.featureCollection){
-   		layerInfo.push({"layer":layer.layerObject,"title":layer.title});
-  }
-});
-	var legendDijit = new esri.dijit.Legend({
-	  map:map,
-	  layerInfos:layerInfo
-	},"legendDiv");
-	legendDijit.startup();
+      require([
+        "dojo/parser",
+        "dojo/ready",
+        "dijit/layout/BorderContainer",
+        "dijit/layout/ContentPane",
+        "dojo/dom",
+        "esri/map", 
+        "esri/urlUtils",
+        "esri/arcgis/utils",
+        "esri/dijit/Legend",
+        "esri/dijit/Scalebar",
+        "dojo/domReady!"
+      ], function(
+        parser,
+        ready,
+        BorderContainer,
+        ContentPane,
+        dom,
+        Map,
+        urlUtils,
+        arcgisUtils,
+        Legend,
+        Scalebar
+      ) {
+        ready(function(){
 
+        parser.parse();
+
+//if accessing webmap from a portal outside of ArcGIS Online, uncomment and replace path with portal URL
+       //arcgisUtils.arcgisUrl = "https://pathto/portal/sharing/content/items";
+        arcgisUtils.createMap("f254021669784a4484f0d341c14d672e","map").then(function(response){
+          //update the app 
+          dom.byId("title").innerHTML = response.itemInfo.item.title;
+          dom.byId("subtitle").innerHTML = response.itemInfo.item.snippet;
+
+          var map = response.map;
+
+
+
+          //add the scalebar 
+          var scalebar = new Scalebar({
+            map: map,
+            scalebarUnit: "english"
+          });
+
+          //add the legend. Note that we use the utility method getLegendLayers to get 
+          //the layers to display in the legend from the createMap response.
+          var legendLayers = arcgisUtils.getLegendLayers(response); 
+          var legendDijit = new Legend({
+            map: map,
+            layerInfos: legendLayers
+          },"legend");
+          legendDijit.startup();
+
+
+        });
+
+
+        });
+
+      });
