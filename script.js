4e9f826a358caabd82804a03903490c4b66d7ddc
geojuju <juliana.elizabeth.t@gmail.com>
2016-03-09 13:15:58 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+	var map;
+	require(["esri/map", "dojo/domReady!"], function(Map) {
+  		map = new Map("mapDiv", {
+    		center: [-97.7115, 30.2705],
+    		zoom: 12,
+    		basemap: "gray-vector"
+  		});
+	});
