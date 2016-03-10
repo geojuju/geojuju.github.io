d39ad74ba74b8169999642f99294ac34da00e859
geojuju <juliana.elizabeth.t@gmail.com>
2016-03-10 10:02:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 	var map;
-	require(["esri/map", "dojo/domReady!"], function(Map) {
+	require(["esri/map", "dojo/domReady!", "esri/dijit/HomeButton",], function(Map, HomeButton) {
   		map = new Map("mapDiv", {
     		center: [-97.7115, 30.2705],
     		zoom: 12,
     		basemap: "gray-vector"
   		});
+  		      var home = new HomeButton({
+        map: map
+      }, "HomeButton");
+      home.startup();
 	});
 
