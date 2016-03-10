a322bf67b15420e8d16a7ca565ef20226c04dbcc
geojuju <juliana.elizabeth.t@gmail.com>
2016-03-10 10:31:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ require([
     "dojo/domReady!"
   ],
   function(
-    Map,
-    FeatureLayer
+    FeatureLayer,
+    Map
+    
   ) {
+  		var featureLayer = new FeatureLayer("https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer");
+
+    map.addLayer(featureLayer);
+	});
   		var map = new Map("mapDiv", {
     		center: [-97.7115, 30.2705],
     		zoom: 12,
     		basemap: "gray-vector"
   		});
-  		var featureLayer = new FeatureLayer("https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer");
-
-    map.addLayer(featureLayer);
-	});
+  	
